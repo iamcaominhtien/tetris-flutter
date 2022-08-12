bb5e1208f92d64dce690e54ff5d72f26c28fbfa7
Cao Minh Tien <iamcaominhtien@gmail.com>
2022-08-13 00:09:44 +0700

    update 13/8/2022: 00:09 AM

--- lib/widgets/tetris_board/tetris_board.dart
+++ lib/widgets/tetris_board/tetris_board.dart
@@ -5,6 +5,7 @@ import 'package:stop_watch_timer/stop_watch_timer.dart';
 import 'package:tetris/components/my_tetris_provider.dart';
 import 'package:tetris/constants/constants.dart';
 
+import 'choose_level_board.dart';
 import 'tetris_board_widget.dart';
 
 class TetrisBoard extends StatefulWidget {
@@ -161,104 +162,3 @@ class _TetrisBoardState extends State<TetrisBoard> {
   }
 }
 
-class ChooseLevelBoard extends StatefulWidget {
-  const ChooseLevelBoard({
-    Key? key,
-    required this.level,
-    required this.callBack,
-  }) : super(key: key);
-
-  final Duration level;
-  final Function(Duration value) callBack;
-
-  @override
-  State<ChooseLevelBoard> createState() => _ChooseLevelBoardState();
-}
-
-class _ChooseLevelBoardState extends State<ChooseLevelBoard> {
-  late Duration level;
-
-  @override
-  void initState() {
-    super.initState();
-    level = widget.level;
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return AlertDialog(
-      title: const Center(
-          child: Text(
-        "CHỌN LEVEL",
-        style: TextStyle(
-          fontWeight: FontWeight.w900,
-        ),
-      )),
-      content: Column(
-        mainAxisSize: MainAxisSize.min,
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          RadioListTile<Duration>(
-            title: const Text('Level 1'),
-            value: Constant.listOfLevel[0],
-            groupValue: level,
-            onChanged: (value) {
-              setState(() {
-                level = value!;
-              });
-              widget.callBack(level);
-            },
-          ),
-          RadioListTile<Duration>(
-            title: const Text('Level 2'),
-            value: Constant.listOfLevel[1],
-            groupValue: level,
-            onChanged: (value) {
-              setState(() {
-                level = value!;
-              });
-              widget.callBack(level);
-            },
-          ),
-          RadioListTile<Duration>(
-            title: const Text('Level 3'),
-            value: Constant.listOfLevel[2],
-            groupValue: level,
-            onChanged: (value) {
-              setState(() {
-                level = value!;
-              });
-              widget.callBack(level);
-            },
-          )
-        ],
-      ),
-      actions: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all(Colors.green),
-              ),
-              onPressed: () => Navigator.of(context).pop('continue'),
-              child: const Text(
-                "Tiếp tục",
-                style: TextStyle(color: Colors.white),
-              ),
-            ),
-            const SizedBox(
-              width: 15,
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.of(context).pop('restart');
-              },
-              child: const Text("Chơi lại"),
-            ),
-          ],
-        )
-      ],
-    );
-  }
-}
